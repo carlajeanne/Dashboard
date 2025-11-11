b78bceeda4ed5b9671397f9ad943d6d87ebbaf5f
Carla Jeanne Golena <carlagolena730@gmail.com>
2025-11-11 20:10:35 +0800

    Styling: Navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,22 +1,33 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { RiDashboardHorizontalLine  } from 'react-icons/ri'
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
-        { name: 'Dashboard', path: '/' },
-        { name: 'Issues', path: '/issues' },
+        { name: 'Dashboard', href: '/' },
+        { name: 'Issues', href: '/issues' },
     ]
 
     return (
         <nav className='flex justify-between items-center p-4 bg-gray-800 text-white'>
-            <Link href="/"><RiDashboardHorizontalLine /></Link>
+            <Link href="/"><RiDashboardHorizontalLine size={20}/></Link>
             <ul className='flex space-x-4'>
                 {links.map((link) => (
                     <li key={link.name}>
                         <Link 
-                            className='text-gray-200 hover:text-amber-300' 
-                            href={link.path}>{link.name}
+                            href={link.href}
+                            className={`${
+                                link.href === currentPath
+                                ? 'text-amber-300'
+                                : 'text-gray-200 hover:text-amber-300'
+                                } transition-colors`}
+                            >
+                                {link.name}
                         </Link>
                     </li>
                 ))}
