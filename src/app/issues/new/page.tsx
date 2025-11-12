cdd055580917ebd8974dff01e73ab93c1c0f8913
Carla Jeanne Golena <carlagolena730@gmail.com>
2025-11-12 18:36:59 +0800

    Add: form submission to create new issue and redirect to issues list on success.

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,25 +1,60 @@
 'use client'
 
-import React from 'react'
+import dynamic from 'next/dynamic'
 import { TextArea, Button } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor"
+import { useForm, Controller } from 'react-hook-form'
 import "easymde/dist/easymde.min.css"
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
+
+interface IssueFormData {
+    title: string
+    description: string
+}
 
 const NewIssuePage = () => {
+    const router = useRouter();
+    const { register, control, handleSubmit } = useForm<IssueFormData>()
+
     return (
-        <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
+        <form
+        className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md"
+        onSubmit=
+            {handleSubmit(async (data) => 
+                await axios.post('/api/issues', data).then(() => {
+                    router.push('/issues');
+                }).catch(() => {
+                    alert('Failed to create issue.')
+                })
+            )}
+        >
             <h1 className="text-2xl font-semibold mb-4">Create New Issue</h1>
+
             <TextArea
+                {...register('title')}
                 placeholder="Issue Title"
                 rows={1}
                 className="w-full p-2 border rounded-lg"
             />
-            <SimpleMDE 
-                placeholder="Description"
-                className="w-full mt-4 p-2 border rounded-lg"
+
+            <Controller
+                name="description"
+                control={control}
+                render={({ field }) => (
+                <SimpleMDE
+                    {...field}
+                    placeholder="Description"
+                    className="w-full mt-4 p-2 border rounded-lg"
+                />
+                )}
             />
-            <Button className='p-5 m-4 justify-items-center'>Submit New Issue</Button>
-        </div>
+
+            <Button className="w-full mt-6 bg-blue-600 text-white hover:bg-blue-700">
+                Submit New Issue
+            </Button>
+        </form>
     )
 }
 
